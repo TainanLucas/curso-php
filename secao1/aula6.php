d438b1bbd5f2d10a72842ffc0d1be14171ef8aee
Tainan Lucas <tainanlucas1533@gmail.com>
2023-09-07 18:37:31 -0300

    primeiro commit

---
+++ secao1/aula6.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Meu primeiro código PHP! Hello Word" . "\n";
+echo "Salve Salve Salve Salve Salve Salve" . "\n";
+$Nome = Tainan;
+$Sobre = Lucas;
+
+echo "Meu nome e $Nome . $Sobre , Moraes";
+
